83cf357e96fb2804dca6db3d9df139e1b6d8a6fa
nengkya <nengkya@yahoo.com>
2025-01-15 07:04:18 +0700

    malloc.h or stdlib.h is a must or there will be error in stdio.h scanf_s _ArgList which is scanf_s looks for allocated space that provided by malloc(); that is inside malloc.h or stdlib.h
    malloc.h is deprecated and Linux specific, on which it defines non-standard functions

--- squareRoot/squareRoot.c
+++ squareRoot/squareRoot.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include"stdio.h"
+/*
+malloc.h or stdlib.h is a must or there will be error in stdio.h scanf_s _ArgList
+which is scanf_s looks for allocated space that provided by malloc(); that is inside malloc.h or stdlib.h
+
+malloc.h is deprecated and Linux specific, on which it defines non-standard functions
+*/
+#include"malloc.h"
+#include"math.h"
 
 int main() {
 
-	printf("Enter a positive value : ");
+    double * positiveValue = malloc(sizeof * positiveValue);
 
+    printf("Enter a positive value: "); scanf_s("%lf", positiveValue);
+
+    printf("The square root of %lf is %lf\n", *positiveValue, sqrt(*positiveValue));
+    
 }
